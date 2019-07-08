fdb2d4be17a06d0cbb2b5c61bb1764a77847cc8a
Terran Blake <terranblake@gmail.com>
2019-07-08 07:42:08 -0700

    remove all comments

--- src/get_sec_data.py
+++ src/get_sec_data.py
@@ -60,11 +60,9 @@ def __xml_to_json(xml_obj, namespaces):
 
 def get_filing_metadata(ticker='AAPL', accession_number="0000320193-17-000070"):
     company = get_company(ticker)
-    # print(company)
 
     base_link = '{}{}/{}/'.format(sec_base, get_cik(ticker), nodash(accession_number))
     filing_link = '{}{}-index.htm'.format(base_link, accession_number)
-    # print('base', base_link)
 
     extensions = pd.read_html(filing_link)
     extensions = pd.concat(extensions)
@@ -79,7 +77,6 @@ def get_filing_metadata(ticker='AAPL', accession_number="0000320193-17-000070"):
         inplace=True
     )
     extensions['url'] = base_link + extensions.fileName
-    # print(extensions)
     extensions = extensions.to_json(orient='records')
     return extensions
 
@@ -90,13 +87,8 @@ def get_xbrl(ticker='AAPL', accession_number="0000320193-17-000070", xbrl_type="
     return filing_detail
 
 
-# TODO :: Implement polling system to check
-#            for new tickers on an interval
-# http://rankandfiled.com/#/data/tickers
 def get_cik(ticker='AAPL'):
     CIK_RE = re.compile(r'.*CIK=(\d{10}).*')
-
-    # print(sec_company_details)
     f = requests.get(sec_company_details.format(ticker), stream=True)
     results = CIK_RE.findall(f.text)
     if len(results):
@@ -106,7 +98,6 @@ def get_cik(ticker='AAPL'):
 
 
 def get_company(ticker='AAPL'):
-    # print('getting identifiers for {}'.format(ticker))
     try:
         f = requests.get(sec_company_details.format(ticker), stream=True)
         t = html.fromstring(f.content)
@@ -144,7 +135,6 @@ def all_exchanges_to_json(path):
         for file in f:
             if '.txt' in file:
                 exchange = file.split('/')[-1].split('.txt')[0]
-                # print(exchange)
                 exchange_to_json(exchange)
 
 
@@ -155,7 +145,6 @@ def exchange_to_json(exchange=None):
 
     companies = {}
     with open(input_path) as open_file:
-        # print('opened {}'.format(input_path))
         open_file.readline()
         line = open_file.readline()
         cnt = 1
@@ -169,21 +158,17 @@ def exchange_to_json(exchange=None):
             cnt += 1
         
         open_file.close()
-        # print('closed {}'.format(input_path))
 
     output_path = '{}/json/{}.json'.format(base_path, exchange)
     output_file = open(output_path, "w")
-    # print('opened {}'.format(output_path))
     output_file.write(json.dumps(companies, indent=4, sort_keys=True))
     output_file.close()
-    # print('closed {}'.format(output_path))
 
 
 def get_filings_by_type(ticker="AAPL", filing_type="10-K", limit="1"):
     cik = get_cik(ticker)
     
     url = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&type={}&dateb=&owner=exclude&start=0&count={}&output=atom'.format(cik, filing_type, limit)
-    # print(url)
     f = requests.get(url.format(cik, filing_type, limit), stream=True)
 
     root = ET.fromstring(f.text)
@@ -191,13 +176,3 @@ def get_filings_by_type(ticker="AAPL", filing_type="10-K", limit="1"):
     root[1][1].text = "filings"
 
     return __xml_to_json(root[1], filings_namespaces)
-
-
-# has ticker lists that are processed
-# using exchange_to_json
-# http://www.eoddata.com/symbols.aspx
-# exchange_to_json('NASDAQ')
-# all_exchanges_to_json('./raw')
-# companies = get_companies_csv().to_json(orient='records')
-# output_file = open('./json/all_companies.json', "w")
-# output_file.write(companies)
